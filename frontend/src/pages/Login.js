f95d6723e966fe60ee25e0c1a82319b6e1902185
MatteoPossamai <mpossamaim@gmail.com>
2021-11-11 16:38:45 +0100

    Finished the login/signup looks page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import '../styles/style.css';
-import LogSub from './LogSub';
+import logo from '../images/user.png';
+
+const LoginContest = React.createContext();
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [checkPassword, setCheckPassword] = useState('');
+    const [name, setName] = useState('');
     const [actMargin, setActMargin] = useState('0px');
 
+    useEffect(() => {
+        setEmail('');
+        setPassword('');
+        setCheckPassword('');
+        setName('');
+    }, [actMargin]);
+    
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Submitted')
+        //console.log('Submitted')
     };
 
     const changeHandlerL = () =>{
@@ -21,8 +32,9 @@ const Login = () => {
         console.log(email);
     }
 
-    return <form className='containerLogin' onSubmit={handleSubmit}>
-        <h1>Login</h1>
+    return <LoginContest.Provider value={{email, setEmail, password, setPassword, checkPassword, setCheckPassword, name, setName}}>
+    <form className='containerLogin' onSubmit={handleSubmit}>
+        <img src={logo} alt="logo" />
 
         <div className="choose">
             <button id="logS" onClick={changeHandlerL}>Login</button>
@@ -30,10 +42,39 @@ const Login = () => {
             <div className={actMargin==='0px' ? "lineaN"  : "lineaM" }></div>
         </div>
 
-        <LogSub email={email} setEmail={setEmail} password={password} setPassword={setPassword} />
-
-        <p>Don't have an account?<button className="sign">Sign up</button></p>
+        {actMargin==='0px' ? <LogSub /> : <SigSub />}
     </form>
+    </LoginContest.Provider>
 }
 
+const LogSub = () => {
+    const l = useContext(LoginContest);
+    console.log(l)
+    return <>
+        <p>Email:</p>
+        <input type="text" name="email" value={l.email} onChange={(e)=> l.setEmail(e.target.value)}/>
+        <p>Password:</p>
+        <input type="password" name="password" value={l.password} onChange={(e)=>l.setPassword(e.target.value)}/>
+
+        <button type="submit" id="sub" >Login</button>
+        <p>Don't have an account?<button className="sign">Sign up</button></p>
+    </>
+}
+
+const SigSub = () => {
+    const l = useContext(LoginContest);
+    console.log(l)
+    return <>
+        <p>Name:</p>
+        <input type="text" name="name" value={l.name} onChange={(e)=> l.setName(e.target.value)}/>
+        <p>Email:</p>
+        <input type="text" name="email" value={l.email} onChange={(e)=> l.setEmail(e.target.value)}/>
+        <p>Password:</p>
+        <input type="password" name="password" value={l.password} onChange={(e)=>l.setPassword(e.target.value)}/>
+        <p>Retype password:</p>
+        <input type="password" name="checkPassword" value={l.checkPassword} onChange={(e)=>l.setCheckPassword(e.target.value)}/>
+
+        <button type="submit" id="sub" >Sign Up</button>
+    </>
+}
 export default Login;
