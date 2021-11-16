a6119a0b0b494d7d1e9246913765bdb41f4f8430
MatteoPossamai <mpossamaim@gmail.com>
2021-11-16 19:35:23 +0100

    Modified and update file routes in backend. Keeped on working on the Editor page. Added the button to change the theme. Stopped in adding the number near the lines. Asked on stackoverflow. Then, something must be ultimated in UI, must create the homepage, but it would be better to start to connect frontend and backend.

--- frontend/src/pages/Editor.js
+++ frontend/src/pages/Editor.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import sun from '../images/sun.png';
+import moon from '../images/moon.png';
 
 const Editor = () => {
     const [text, setText] = useState('//Your code');
@@ -15,17 +17,27 @@ const Editor = () => {
 
     const handleChanges = (e) => {
         setText(e.target.value);   
+    }
+
+    const countLine = () => {
         let lines = text.split(/\r|\r\n|\n/);
         let count = lines.length;
-        console.log(count)
+        return  count;
     }
+
     return <>
         <form className="editor">
-            <div className="opened">index.html  x</div>
-            <div>{/*Pull here number of lines*/}</div>
+            <div className="opened">  index.html  </div>
+            <div>
+                {<ul>
+                    {Array.from(Array(countLine), (e, i) => {
+                    return <li key={i}>{i}</li>
+                    })}</ul>}
+            </div>
             <textarea name="code" value={text} className="editorText" onChange={handleChanges} spellCheck="false" 
             id = {bg}></textarea>
-            <button onClick={changeStyle} className="buttonTheme">Change theme</button>
+            <button onClick={changeStyle} className="buttonTheme"><img src={sun} alt="sun" className="obj"/><img src={moon} alt="moon" className="obj" id="moon"/>
+            <div className={bg==='dark' ? "circleDark" : "circleLight"}></div></button>
         </form>
     </>
 }
