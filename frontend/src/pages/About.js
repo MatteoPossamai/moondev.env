e9ad8da66cd8b874b20959bc88293b5d0edb91db
MatteoPossamai <61617283+MatteoPossamai@users.noreply.github.com>
2021-11-23 12:56:58 +0100

    Created About page
    Also need something from where you can set group settings.

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,17 @@
+import Footer from '.Footer';
+
+const About = () => {
+    return (<>
+           <h1>moondev.env - About</h1>
+            
+            <p>moondev.env is a free, online, open source, collaborative development enviroment. Here, you can register your account, and log
+            in from every computer in the internet. You will be part of a group, or a lonely develper. You will have a free group chat, for
+            chat with your coworkers, and interact with them. You will obviusly have a feature-rich code editor, that will change in real time
+            for all the users connected to that specific file, so pair programming become possible even from remote, and with low-latency
+            technology. Each group will have a file dashboard, so everyone can see the same file and workspace, making a sort of git, but real-time,
+            and with no needing for </p>
+           <Footer />
+           </>);
+}
+
+export default About;
